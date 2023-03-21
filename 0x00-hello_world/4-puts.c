1fd10bf228f211a8ce3ad8509437fc88879f65c7
zinobravo <exinobravo@gmail.com>
2023-03-21 06:19:23 -0700

    my first c program with betty style

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line using the put function
+ * Return:0(Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like biulding a multilingual puzzle");
+	return (0);
+}
