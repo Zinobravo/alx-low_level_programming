de5e6db841245fd2ce87dd89d9c3d68d13249f8a
zinobravo <exinobravo@gmail.com>
2023-03-22 15:29:00 -0700

    101-quote.c updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - C program that prints a line to the standard error
- * Return: 1 (success)
+ * main - C program that prints a line to the stabdard error
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
+}
